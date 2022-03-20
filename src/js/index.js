7a20ecfb783f11abd37d919bbf7ad8fef8912110
Silas Lago <juanfsa@live.com>
2022-03-20 20:28:28 -0300

    curriculo div retornou guys

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
 const aboutMe = document.getElementById('sobre-mim')
+const curriculo = document.getElementById('curriculo')
 const projects = document.getElementById('projetos')
 
 aboutMe.onclick = function() {
     document.getElementById('sobre-mim-div').scrollIntoView()
+}
+
+curriculo.onclick = function() {
+    document.getElementById('curriculo-div').scrollIntoView()
 }
 
 projects.onclick = function() {
